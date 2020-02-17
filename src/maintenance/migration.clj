5be0579d4765fc7a71a30adb4974405c3837d5c9
Thierry Marianne <thierry@marianne.io>
2020-02-17 21:43:31 +0100

    Reformat code

--- src/maintenance/migration.clj
+++ src/maintenance/migration.clj
@@ -10,22 +10,24 @@
   [status]
   (let [hash (sha256 (str (:screen-name status) "|"
                           (:status-twitter-id status)))]
-  {:legacy_id    (:id status)
-   :hash         hash
-   :text         (:text status)
-   :screen_name  (:screen-name status)
-   :avatar_url   (:avatar-url status)
-   :document_id  (:status-twitter-id status)
-   :document     (:document status)
-   :published_at (:created-at status)}))
+    {:legacy_id    (:id status)
+     :hash         hash
+     :text         (:text status)
+     :screen_name  (:screen-name status)
+     :avatar_url   (:avatar-url status)
+     :document_id  (:status-twitter-id status)
+     :document     (:document status)
+     :published_at (:created-at status)}))
 
 (defn migrate-status-to-publications
   []
   (let [{status-model      :status
          publication-model :publication} (get-entity-manager (:database env))
         status (status/find-unpublished-statuses status-model 100000)
-        publication-props (pmap status-to-publication status)]
-    (publication/bulk-insert-of-publication-props
-      publication-props
-      publication-model
-      status-model)))
+        publication-props (pmap status-to-publication status)
+        publications (publication/bulk-insert-of-publication-props
+                       publication-props
+                       publication-model
+                       status-model)]
+    (if (pos? (count publications))
+      (migrate-status-to-publications))))
